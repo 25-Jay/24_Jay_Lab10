ee0c3b2e8219dd9b3e0f1bd5e505296d474c73f3
25-Jay <ang.ying.jie.jay@Gmail.com>
2024-01-24 11:30:44 +0800

    added unity project

---
+++ 24_Jay_Lab10_Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Text scoreText;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if (transform.position.y >= 10)
+        {
+            transform.position = new Vector3(transform.position.x, 10, transform.position.z);
+        }
+
+        else if (transform.position.y <= 2)
+        {
+            transform.position = new Vector3(transform.position.x, 2, transform.position.z);
+        }
+
+        scoreText.text = "Score: " + score;
+
+
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            
+            Destroy(collision.gameObject);
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+     public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+
+            score += 10;
+        }
+    }
+    
+}
